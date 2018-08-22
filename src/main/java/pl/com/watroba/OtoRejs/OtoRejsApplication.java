98ce040c29520823e9285c1592fc940847fc3a63
Paulina Wątroba <paulina@watroba.com.pl>
2018-08-22 23:27:30 +0200

    New Project with Maven

---
+++ src/main/java/pl/com/watroba/OtoRejs/OtoRejsApplication.java
@@ -0,0 +1,12 @@
+package pl.com.watroba.OtoRejs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtoRejsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtoRejsApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/com/watroba/OtoRejs/OtoRejsApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.com.watroba.OtoRejs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OtoRejsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
